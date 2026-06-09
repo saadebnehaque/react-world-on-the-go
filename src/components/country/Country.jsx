4a53b1e65ec33f6c6149f029f5d7619a2785c7ff
Saad <sebnehaque@gmail.com>
2026-06-10 00:29:38 +0600

    something added

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
+
 // import React from 'react'; 
 const Country = ({ country }) => {
+    const [isVisited, setIsVisited] = useState(false)
+
     return (
         <>
-            <div className="country">
+            <div className={`country ${isVisited && 'visitedCountry'}`}>
                 <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
                 <h1 style={{ color: 'white' }}>
                     {country.name.common}
@@ -22,15 +26,19 @@ const Country = ({ country }) => {
                     Region: <span style={{ color: 'white' }}>
                         {country.region.region}
                     </span>
-                    <h3>
-                        Languages:
-                        <span style={{ color: "white" }}>
-                            {country.languages?.languages &&
-                                Object.values(country.languages.languages).join(", ")}
-                        </span>
-                    </h3>
-                </h3>
 
+                </h3>
+                <h3>
+                    Languages:
+                    <span style={{ color: "white" }}>
+                        {country.languages?.languages &&
+                            Object.values(country.languages.languages).join(", ")}
+                    </span>
+                </h3>
+                <h3>
+                    Area: <span style={{ color: "white" }}>{country.area.area > 300000 ? 'Big Country' : 'Small Country'}</span>
+                </h3>
+                <button onClick={() => setIsVisited(!isVisited)}>{isVisited ? 'Visited' : 'Not Visited'}</button>
             </div>
         </>
     );
