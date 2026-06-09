3ac0051dd320e9f9c9cb295eb95104d6c03b1827
Saad <sebnehaque@gmail.com>
2026-06-09 12:58:49 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Countries from './components/countries/countries'
+
+function App() {
+
+  return (
+   <>
+   <h1>react World on the go</h1>
+   <Countries></Countries>
+   </> 
+  )
+}
+
+export default App
