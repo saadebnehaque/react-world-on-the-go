b66a44dd4369d3fbe3cf7e33997dc19ecb0a3759
Saad <sebnehaque@gmail.com>
2026-06-11 12:50:35 +0600

    Some design chainge

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -7,38 +7,47 @@ const Country = ({ country }) => {
     return (
         <>
             <div className={`country ${isVisited && 'visitedCountry'}`}>
-                <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
-                <h1 style={{ color: 'white' }}>
-                    {country.name.common}
+                <div>
 
-                </h1>
-                <h3>
-                    Official name: <span style={{ color: 'white' }}>
-                        {country.name.official}
-                    </span>
-                </h3>
-                <h3>
-                    Capital: <span style={{ color: 'white' }}>
-                        {country.capital.capital}
-                    </span>
-                </h3>
-                <h3>
-                    Region: <span style={{ color: 'white' }}>
-                        {country.region.region}
-                    </span>
+                    <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+                </div>
+                <div>
 
-                </h3>
-                <h3>
-                    Languages:
-                    <span style={{ color: "white" }}>
-                        {country.languages?.languages &&
-                            Object.values(country.languages.languages).join(", ")}
-                    </span>
-                </h3>
-                <h3>
-                    Area: <span style={{ color: "white" }}>{country.area.area > 300000 ? 'Big Country' : 'Small Country'}</span>
-                </h3>
-                <button onClick={() => setIsVisited(!isVisited)}>{isVisited ? 'Visited' : 'Not Visited'}</button>
+                    <h1 style={{ color: 'white' ,lineHeight:'110%'}}>
+                        {country.name.common}
+
+                    </h1>
+                    <h3>
+                        Official name: <span style={{ color: 'white' }}>
+                            {country.name.official}
+                        </span>
+                    </h3>
+                    <h3>
+                        Capital: <span style={{ color: 'white' }}>
+                            {country.capital.capital}
+                        </span>
+                    </h3>
+                    <h3>
+                        Region: <span style={{ color: 'white' }}>
+                            {country.region.region}
+                        </span>
+
+                    </h3>
+                    <h3>
+                        Languages:
+                        <span style={{ color: "white" }}>
+                            {country.languages?.languages &&
+                                Object.values(country.languages.languages).join(", ")}
+                        </span>
+                    </h3>
+                    <h3>
+                        Area: <span style={{ color: "white" }}>{country.area.area > 300000 ? 'Big Country' : 'Small Country'}</span>
+                    </h3>
+                </div>
+                <div>
+
+                    <button onClick={() => setIsVisited(!isVisited)}>{isVisited ? 'Visited' : 'Not Visited'}</button>
+                </div>
             </div>
         </>
     );
